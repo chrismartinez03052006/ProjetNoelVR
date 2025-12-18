2334fbb4869a9f47d2f26a095839c187d3990fbf
Yuu-NiKorn <yuuyunikorn@gmail.com>
2025-12-18 15:54:47 +0100

    jme coupe le zob si ca marche po
    (normalement l'ui est connecte la)

--- Projet VR/Assets/ScriptChris/RecipeManager.cs
+++ Projet VR/Assets/ScriptChris/RecipeManager.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using System;
 
 public class RecipeManager : MonoBehaviour
 {
     [SerializeField] private int currentStepIndex = 0;
+
+    public event Action<int> OnStepCompleted;
 
     public bool CanDoStep(int stepIndex) => stepIndex == currentStepIndex;
 
     public void CompleteStep(int stepIndex)
     {
         if (stepIndex != currentStepIndex) return;
+
         currentStepIndex++;
-        Debug.Log($" Étape {stepIndex} terminée. Prochaine étape: {currentStepIndex}");
+
+        // J'ai rajj ça chris si jamais 
+        OnStepCompleted?.Invoke(currentStepIndex);
+
+        Debug.Log($"Étape {stepIndex} terminée. Prochaine étape: {currentStepIndex}");
     }
 }
